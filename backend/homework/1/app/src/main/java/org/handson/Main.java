be29be8244de504016ce0fb4c95f14066a5e53de
Mathangi Krishnamurthy <155649491+mathangi-krishnamurthy@users.noreply.github.com>
2024-01-09 00:21:18 +0530

    Update Main.java

--- backend/homework/1/app/src/main/java/org/handson/Main.java
+++ backend/homework/1/app/src/main/java/org/handson/Main.java
@@ -1,92 +1,126 @@
 package org.handson;
 
 import java.util.Scanner;
-
 import static java.lang.System.exit;
 
 public class Main {
-    Student getStudentDetail() {
+    Student getStudentDetail()
+    {        System.out.print("\033[H\033[2J");
 
         Student student = new Student();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("\nEnter Student ID: ");
 
-
-        int id = 1;
+        int id = sc.nextInt();
         student.setID(id);
-
-        String name = "mathangi";
+sc.nextLine();
+        System.out.println("\nEnter Student Name: ");
+        String name = sc.nextLine();
         student.setName(name);
 
-
-        int age = 21;
+        System.out.println("\nEnter Student Age: ");
+        int age = sc.nextInt();
         student.setAge(age);
 
-
-        char letter = 'A';
+        System.out.println("\nEnter Student Grade: ");
+        String grade = sc.next();
+        char letter = grade.charAt(0);
         student.setGrade(letter);
         return student;
 
     }
 
-    void Update(StudentRepo studentRepoobj) {
-
-
-        int ID = 1;
-        Student student = studentRepoobj.getStudent(ID);
-        if (student == null) {
-            System.out.println("User not found");
-            return;
+    void update(StudentRepo studentRepoobj)
+    {
+    System.out.print("\033[H\033[2J");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("\nEnter the ID of the student to be updated:");
+        int iD = sc.nextInt();
+        System.out.println("\nChoose one of the following:\n1: Update the age\n2:Update the grade");
+        Student student = studentRepoobj.getStudent(iD);
+        if(student==null) {System.out.println("User not found");return;}
+        int option = sc.nextInt();
+        switch(option)
+        {
+            case 1: System.out.println("\nEnter the new value of age: ");
+                    int newage =  sc.nextInt();
+                    studentRepoobj.updateStudentAge(newage,student); break;
+            case 2: System.out.println("\nEnter the new value of the grade");
+                String newgrade =  sc.next();
+                char letter  = newgrade.charAt(0);
+                studentRepoobj.updateStudentAge(letter,student); break;
+            default: System.out.println("\nInvalid option, Try Again") ;break;
         }
-        int newage = 45;
-        studentRepoobj.updateStudentAge(newage, student);
-
-        char letter = 'B';
-        studentRepoobj.updateStudentGrade(letter, student);
-
-
     }
 
-    void Retrieve(StudentRepo studentRepoObj) {
+    void retrieve(StudentRepo studentRepoObj)
+    {
+        System.out.println("\nRetrieve by one of the following:\n1: ID\n2:Name");
+        Scanner sc = new Scanner(System.in);
 
+        int option = sc.nextInt();
+        switch(option)
+        {
+            case 1:
+                System.out.println("\nEnter the ID of the student :");
+                int iD = sc.nextInt();
+                Student st1 = studentRepoObj.getStudent(iD);
+                if(st1!=null)
+                    System.out.println("\nID: "+st1.getID()+" Name: "+st1.getName()+" Grade:"+st1.getGrade()+" Age:"+st1.getAge()+" ");
 
-        int ID = 1;
-        Student st1 = studentRepoObj.getStudent(ID);
-        if (st1 != null)
-            CustomLogging.getLoggerCustom().info("\nID: " + st1.getID() + " Name: " + st1.getName() + " Grade:" + st1.getGrade() + " Age:" + st1.getAge() + " ");
+                else
+                    System.out.println("Unable to find user");
 
-        else
-            System.out.println("Unable to find user");
+                break;
+            case 2:      sc.nextLine();
+                System.out.println("\nEnter the name of the student:");
+                String name = sc.nextLine();
+               Student st = studentRepoObj.getStudent(name);
+               if(st!=null)
+                System.out.println("\nID: %s"+st.getID()+" Name: "+st.getName()+" Grade:"+st.getGrade()+" Age:"+st.getAge()+" ");
+               else
+                   System.out.println("Unable to find user");
+                break;
+            default: System.out.println("\nInvalid option, Try Again") ;break;
 
-
-        String name = "mathangi";
-        Student st = studentRepoObj.getStudent(name);
-        if (st != null)
-            CustomLogging.getLoggerCustom().info("\nID: " + st.getID() + " Name: " + st.getName() + " Grade:" + st.getGrade() + " Age:" + st.getAge() + " ");
-        else
-            CustomLogging.getLoggerCustom().error("Unable to find user");
-
+        }
 
     }
-
     public static void main(String[] args) {
-
+        char contin = 'n';
 
         Main driver = new Main();
 
-        try {
-            StudentRepo studentRepoobj = new StudentRepo();
+        System.out.println("Hi there! Welcome to the Kickdrum Student Portal");
+        do {   System.out.println("\nChoose one of the following to get what you want done!\n1:Add student\n2:Update Student Info\n3:Retrieve Student\n4.Exit");
+try {
+    StudentRepo studentRepoobj = new StudentRepo();
+    Scanner sc = new Scanner(System.in);
+    int option = sc.nextInt();
 
+    switch (option) {
+        case 1:
             Student student = driver.getStudentDetail();
             studentRepoobj.addStudent(student);
+            break;
+        case 2:
+            driver.update(studentRepoobj);
+            break;
+        case 3:
+            driver.retrieve(studentRepoobj);
+            break;
+        case 4:
+            exit(0);
+        default:
+            System.out.println("\nInvalid option, Exiting..");
 
-            driver.Update(studentRepoobj);
-
-            driver.Retrieve(studentRepoobj);
-
-
-        } catch (Exception e) {
-            System.out.println("Something went wrong\n");
-        }
-
+    }
+    System.out.println("Continue?");
+    String str = sc.next();
+    contin = str.charAt(0);
+}catch(Exception e)
+{System.out.println("Some error occured, press yes to continue, n to exit\n");}
+        }while(contin != 'n');
 
     }
 }
