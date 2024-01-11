56fc2cfd7bc5fd846f1b8a84402770a682c97a19
mathangi-krishnamurthy <mathangi.krishnamurthy@kickdrumtech.com>
2024-01-11 23:51:35 +0530

    added documentation in some files

--- backend/homework/4/src/main/java/org/handson/Question3/PubDateAscComparator.java
+++ backend/homework/4/src/main/java/org/handson/Question3/PubDateAscComparator.java
@@ -3,6 +3,16 @@ package org.handson.Question3;
 import org.handson.MyLogger;
 import java.util.Comparator;
  class PubDateAscComparator implements Comparator<Book>{
+    /**
+     * Compares two Book objects based on their year and title.
+     *
+     * @param  book1  the first Book object to be compared
+     * @param  book2  the second Book object to be compared
+     * @return        an integer value indicating the comparison result:
+     *                - a negative value if book1's year is less than book2's year
+     *                - zero if book1's year is equal to book2's year and their titles are the same
+     *                - a positive value if book1's year is greater than book2's year
+     */
     public int compare(Book book1,Book book2)
     {
         MyLogger.customLogger("book1 year: "+book1.getYear()+" book2 year: "+book2.getYear()+"", "INFO");
--- backend/homework/4/src/main/java/org/handson/Question3/PubDateDescComparator.java
+++ backend/homework/4/src/main/java/org/handson/Question3/PubDateDescComparator.java
@@ -4,6 +4,15 @@ import org.handson.MyLogger;
 import java.util.Comparator;
 
  class PubDateDescComparator implements Comparator<Book>{
+      /**
+       * Compares two books based on their years and titles.
+       *
+       * @param  book1  the first book to compare
+       * @param  book2  the second book to compare
+       * @return        a negative integer if book1's year is greater than book2's year,
+       *                a positive integer if book1's year is less than book2's year,
+       *                or zero if the years are equal and the titles are compared lexicographically
+       */
       public int compare(Book book1,Book book2)
     {
         MyLogger.customLogger("book1 year: "+book1.getYear()+" book2 year: "+book2.getYear()+"", "INFO");
