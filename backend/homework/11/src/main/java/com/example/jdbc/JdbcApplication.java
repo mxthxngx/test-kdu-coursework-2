a831073220b9f6dd8e473c5c28c9a4d375e5c3a7
mathangi-krishnamurthy <mathangi.krishnamurthy@kickdrumtech.com>
2024-01-31 10:54:56 +0530

    uncommented test file

--- backend/homework/11/src/main/java/com/example/jdbc/JdbcApplication.java
+++ backend/homework/11/src/main/java/com/example/jdbc/JdbcApplication.java
@@ -1,15 +1,15 @@
-// package com.example.jdbc;
+package com.example.jdbc;
 
-// import org.springframework.boot.SpringApplication;
-// import org.springframework.boot.autoconfigure.SpringBootApplication;
-// import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-// @SpringBootApplication
-// @EnableTransactionManagement
-// public class JdbcApplication {
+@SpringBootApplication
+@EnableTransactionManagement
+public class JdbcApplication {
 
-// 	public static void main(String[] args) {
-// 		SpringApplication.run(JdbcApplication.class, args);
-// 	}
+	public static void main(String[] args) {
+		SpringApplication.run(JdbcApplication.class, args);
+	}
 
-// }
+}
