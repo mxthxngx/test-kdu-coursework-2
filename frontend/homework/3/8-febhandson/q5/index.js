11824818b7f0c48ce6f7ca62f9906da0aa78f968
Mathangi Krishnamurthy <155649491+mathangi-krishnamurthy@users.noreply.github.com>
2024-02-08 14:43:40 +0530

    Update index.js

--- frontend/homework/3/8-febhandson/q5/index.js
+++ frontend/homework/3/8-febhandson/q5/index.js
@@ -1,26 +1,50 @@
-player = {
+const player = {
     firstName: "Leo",
     lastName: "Messi",
     address: {
-    country: "Spain",
-    city: "Barcelona",
+        country: "Spain",
+        city: "Barcelona",
     },
     careerInfo: {
-    fcBarcelona: {
-    appearances: 780,
-    goals: {
-    premierLeagueGoals: 590,
-    championsLeagueGoals: 50,
+        fcBarcelona: {
+            appearances: 780,
+            goals: {
+                premierLeagueGoals: 590,
+                championsLeagueGoals: 50,
+            },
+        },
     },
-    },
-    },
-    };
-console.log("Only keys")
-console.log(Object.keys(player));
+};
 
+console.log("Only keys:");
+console.log(getAllKeys(player));
 
-const getObjectValues = (obj) => (obj && typeof obj === 'object')
- ?Object.values(obj).map(getObjectValues).flat()
- : [obj]
-console.log("Only values")
-console.log(getObjectValues(player));
+console.log("Only values:");
+console.log(getAllValues(player));
+
+function getAllKeys(obj) {
+    let keys = [];
+
+    for (let key in obj) {
+        keys.push(key);
+        if (typeof obj[key] === 'object') {
+            keys = keys.concat(getAllKeys(obj[key]));
+        }
+    }
+
+    return keys;
+}
+
+function getAllValues(obj) {
+    let values = [];
+
+    for (let key in obj) {
+        if (typeof obj[key] === 'object') {
+            values = values.concat(getAllValues(obj[key]));
+        } else {
+            values.push(obj[key]);
+        }
+    }
+
+    return values;
+}
