c57803737f961688623f0e986fff56090840712d
mathangi-krishnamurthy <mathangi.krishnamurthy@kickdrumtech.com>
2024-03-01 13:09:27 +0530

    added submit

--- frontend/assessments/3/ui/Home.tsx
+++ frontend/assessments/3/ui/Home.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch, RootState } from "../redux/Store";
 import { Addons } from "./Addons";
@@ -7,28 +8,56 @@ import { useEffect } from "react";
 import { fetchRoomData } from "../redux/Thunk";
 import { Price } from "./Price";
 
-export  function Home() {
-    const dispatch:AppDispatch=useDispatch();
-    useEffect(()=>{
-        try{
+export function Home() {
+    const dispatch: AppDispatch = useDispatch();
+    const [submittedValues, setSubmittedValues] = useState<any>(null); // State to store submitted values
+
+    useEffect(() => {
+        try {
             dispatch(fetchRoomData());
+        } catch (error) {
+            console.error(error);
         }
-        catch(error)
-        {
-            console.error(error)
+    }, []);
+
+    const roomItems = useSelector((state: RootState) => state.rooms.roomItems);
+    const currentlySelectedRoom = useSelector((state: RootState) => state.rooms.currentlySelectedRoom);
+    const addonsSelected = useSelector((state: RootState) => state.rooms.addonsSelected);
+    const dates = useSelector((state: RootState) => state.rooms.dates);
+
+    const handleSubmit = () => {
+        if (currentlySelectedRoom && addonsSelected.length > 0 && dates !== 0) {
+            setSubmittedValues({
+                room: currentlySelectedRoom,
+                addons: addonsSelected,
+                dates: dates
+            });
+        } else {
+            alert("Please fill all the fields before submitting.");
         }
-    },[])
+    };
 
+    return (
+        <div>
+            <Room />
+            <DateSelection />
+            <Addons />
+            <Price />
+            <button onClick={handleSubmit}>Submit</button>
 
-    const roomItems=useSelector((state:RootState)=>state.rooms.roomItems)
-    console.log("Roomitems: ",roomItems,"type",roomItems.map((room)=>room.name))
-  return (
-    <div>
-        <Room/>
-        <DateSelection/>
-        <Addons/>
-        <Price/>
-
-    </div>
-  )
+            {submittedValues && (
+                <div>
+                    <h2>Submitted Values</h2>
+                    <p>Room: {submittedValues.room.name}</p>
+                    <p>Addons:</p>
+                    <ul>
+                        {submittedValues.addons.map((addon: any, index: number) => (
+                            <li key={index}>{addon.name}</li>
+                        ))}
+                    </ul>
+                    <p>Dates: {submittedValues.dates}</p>
+                </div>
+            )}
+        </div>
+    );
 }
